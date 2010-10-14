577adadb54161043eda10e44cd9c7b077f6cc583
ujihisa <ujihisa@gmail.com>
2010-10-14 01:21:32 -0700

    Vim indentation

--- package_loader.py
+++ package_loader.py
@@ -8,17 +8,17 @@ from google.appengine.tools import bulkloader
 from vimjolts import Package
 
 class PackageLoader(bulkloader.Loader):
-  def __init__(self):
-    bulkloader.Loader.__init__(self, 'Package',
-                               [('name', str),
-                                ('version', str),
-                                ('url', str),
-                                ('description', lambda x: x.decode("utf-8", "")),
-                                ('packer', lambda x: users.User(email=x)),
-                                ('requires', str),
-                                ('installer', str),
-                                ('timestamp', lambda x: datetime.datetime.strptime(x, '%Y/%m/%d').date())
-                               ])
+    def __init__(self):
+        bulkloader.Loader.__init__(
+                self, 'Package', [
+                    ('name', str),
+                    ('version', str),
+                    ('url', str),
+                    ('description', lambda x: x.decode("utf-8", "")),
+                    ('packer', lambda x: users.User(email=x)),
+                    ('requires', str),
+                    ('installer', str),
+                    ('timestamp', lambda x: datetime.datetime.strptime(x, '%Y/%m/%d').date())])
 
 loaders = [PackageLoader]
 # vim:set et ts=4 sw=4 sts=4 :
